57aaf1315a93e7491a26794ea611d8d2a5789069
andreibuntsev <andrew.buntsev@gmail.com>
2020-04-14 17:57:23 +1000

    Added hashtags functionality

--- main.js
+++ main.js
@@ -1,52 +1,59 @@
+//HASHTAGS
+const HASHTAG_CRAWLING = 'Crawling';
+const HASHTAG_EXPLOSION = 'Explosion';
+const HASHTAG_BIG = 'Big';
+const HASHTAG_SPIN = 'Spin';
+
 const gifs = [
-  "Funnygif",
-  "Piston",
-  "Chest",
-  "Rolling",
-  "love",
-  "crying",
-  "bye",
-  "bear",
-  "database",
-  "System",
-  "World",
-  "temperature",
-  "Clock",
-  "Icon technology gif",
-  "Coin",
-  "Fall",
-  "Healthy",
-  "Furnace",
-  "Cobblestone",
-  "Lava",
-  "Cauldron",
-  "Ingots",
-  "Message",
-  "StoneBricks",
-  "Repeater",
-  "RepeaterSwitch",
-  "Minecraft-tnt-explosion",
-  "Torch",
-  "Lantern",
-  "Planks",
-  "Plank.Slab",
-  "usb",
-  "Scaffolding",
-  "Bomb",
-  "Lamp",
-  "Value",
-  "Dust",
-  "Boom",
-  "chicken",
-  "spin",
-  "kiwi",
-  "lemon",
-  "eat",
-  "hello",
+  { name: "Funnygif", tags: [] },
+  { name: "Piston", tags: [] },
+  { name: "Chest", tags: [] },
+  { name: "Rolling", tags: [] },
+  { name: "love", tags: [] },
+  { name: "crying", tags: [] },
+  { name: "bye", tags: [] },
+  { name: "bear", tags: [HASHTAG_CRAWLING] },
+  { name: "database", tags: [] },
+  { name: "System", tags: [] },
+  { name: "World", tags: [] },
+  { name: "temperature", tags: [] },
+  { name: "Clock", tags: [] },
+  { name: "Icon technology gif", tags: [] },
+  { name: "Coin", tags: [] },
+  { name: "Fall", tags: [] },
+  { name: "Healthy", tags: [] },
+  { name: "Furnace", tags: [] },
+  { name: "Cobblestone", tags: [] },
+  { name: "Lava", tags: [] },
+  { name: "Cauldron", tags: [] },
+  { name: "Ingots", tags: [] },
+  { name: "Message", tags: [] },
+  { name: "StoneBricks", tags: [] },
+  { name: "Repeater", tags: [] },
+  { name: "RepeaterSwitch", tags: [] },
+  { name: "Minecraft-tnt-explosion", tags: [] },
+  { name: "Torch", tags: [] },
+  { name: "Lantern", tags: [] },
+  { name: "Planks", tags: [] },
+  { name: "Plank.Slab", tags: [] },
+  { name: "usb", tags: [] },
+  { name: "Scaffolding", tags: [] },
+  { name: "Bomb", tags: [HASHTAG_BIG, HASHTAG_EXPLOSION] },
+  { name: "Lamp", tags: [] },
+  { name: "Value", tags: [] },
+  { name: "Dust", tags: [] },
+  { name: "Boom", tags: [HASHTAG_EXPLOSION] },
+  { name: "chicken", tags: [] },
+  { name: "spin", tags: [] },
+  { name: "kiwi", tags: [] },
+  { name: "lemon", tags: [HASHTAG_SPIN] },
+  { name: "eat", tags: [] },
+  { name: "hello", tags: [] }
 ].map(i => {
-  const title = i.toLowerCase();
-  return title.slice(0, 1).toUpperCase() + title.slice(1);
-}).sort();
+  let title = i.name.toLowerCase();
+  title = title.slice(0, 1).toUpperCase() + title.slice(1);
+  return { name: title, tags: i.tags };
+}).sort((a, b) => a.name.localeCompare(b.name));
 
 const PAGE_SIZE = 10;
 
@@ -62,7 +69,8 @@ function loadGifs() {
   container.innerHTML = '';
 
   for (let i = (state.pageNumber - 1) * PAGE_SIZE; i < Math.min(gifs.length, state.pageNumber * PAGE_SIZE); i++) {
-    let gifName = gifs[i];
+    let gifName = gifs[i].name;
+    console.log(gifs[i]);
     const slideContainer = document.createElement("div");
     slideContainer.classList.add("slideContainer");
 
@@ -79,7 +87,11 @@ function loadGifs() {
 
     const title = document.createElement('span');
     title.classList.add('slideTitle');
-    title.innerText = gifName;
+    let titleText = gifName;
+    if (gifs[i].tags.length > 0) {
+      titleText += ' # ' + gifs[i].tags.reduce((acc, el) => `${acc} # ${el}`);
+    }
+    title.innerText = titleText;
 
     slideContainer.appendChild(a);
     slideContainer.appendChild(title);
